b3db3d09035e8526b811e6236329cdfa957e7e4d
liruimin <fighting@liruimindeMacBook-Pro.local>
2021-04-07 11:47:22 +0800

    app页面修改

--- plaugin/package/index.js
+++ plaugin/package/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Select } from 'antd';
+import './index.css';
+const { Option } = Select;
 const testComponnet = () => {
     console.log('abc')
     return (
         <div>
-            这个是主程序12 www eee2211223v风格
+            <span>B应用引入的组建：</span>
+            <Select defaultValue="lucy" style={{ width: 120 }}>
+                <Option value="jack">下拉1</Option>
+                <Option value="lucy">下拉2</Option>
+                <Option value="disabled" disabled>
+                   下拉3
+                </Option>
+                <Option value="Yiminghe">下拉4</Option>
+            </Select>
         </div>
     )
 }
